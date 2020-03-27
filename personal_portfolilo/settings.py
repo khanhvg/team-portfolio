4f80197075131ae530e43f6c6cb7c1c84cd07065
khanhvg <khanhvg99@gmail.com>
2020-03-27 16:11:03 +0700

    Change setting

--- personal_portfolilo/settings.py
+++ personal_portfolilo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l@1h=c0@l#5ujz)=(=i74m23(m#%29qb28x2kj4#db(8+uk$8y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
